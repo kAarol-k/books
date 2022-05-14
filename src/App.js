67d3af993611c8f13e791dfeb07e5fb722b4f23e
Karol <74067027+kAarol-k@users.noreply.github.com>
2022-05-14 13:23:28 +0200

    Data Fetch

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import axios from 'axios';
 import React from 'react';
 function App() {
   const [book, setBook] = useState('');
-  const [resoult, setResoult] = useState([]);
+  const [result, setResult] = useState([]);
+
+  const api2 = `https://www.googleapis.com/books/v1/volumes?q=${book}&key=${process.env.REACT_APP_API_KEY}&maxResults=40`;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(book);
+    axios.get(api2).then((data) => {
+      setResult(data.data.items);
+      console.log(data);
+    });
   };
   return (
     <div className="App">
@@ -28,6 +34,18 @@ function App() {
           send
         </button>
       </form>
+
+      <div>
+        {result.map((book) => (
+          <div>
+            <p>{book.volumeInfo?.title}</p>
+            <p>{book.volumeInfo?.publishedDate}</p>
+            <p>{book.volumeInfo?.description}</p>
+            <p>{book.volumeInfo?.authors}</p>
+            <img src={book.volumeInfo?.imageLinks?.thumbnail} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
