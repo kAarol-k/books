0436f90a4ea7061da0640631c1c67afe321a174c
Karol <74067027+kAarol-k@users.noreply.github.com>
2022-05-15 11:55:16 +0200

    redux-persist

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </Router>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import bookReducer from './Books';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, bookReducer);
+
 export default configureStore({
   reducer: {
-    books: bookReducer,
+    books: persistedReducer,
   },
 });
